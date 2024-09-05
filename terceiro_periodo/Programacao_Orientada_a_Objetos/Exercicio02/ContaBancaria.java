57f418c1d1b62e304682c96c3e1d6c4458bd160d
Milton Nunes <miltonnunesc@hotmail.com>
2024-09-05 15:57:35 -0300

    Exercicio 02

---
+++ terceiro_periodo/Programacao_Orientada_a_Objetos/Exercicio02/ContaBancaria.java
@@ -0,0 +1,20 @@
+public class ContaBancaria {
+    int numero;
+    String titular;
+    double saldo;
+
+    void Depositar(double deposito) {
+        System.out.printf("Deposito de R$%.2f realizado.\n\n", deposito);
+        saldo += deposito;
+    }
+
+    void Sacar(double saque){
+        System.out.printf("Saque de R$%.2f realizado.\n\n", saque);
+        saldo -= saque;
+    }
+
+    double ConsultarSaldo(){
+        System.out.printf("Saldo atual: R$%.2f\n\n", saldo);
+        return saldo;
+    }
+}
---
+++ terceiro_periodo/Programacao_Orientada_a_Objetos/Exercicio02/TestaContaBancaria.java
@@ -0,0 +1,13 @@
+public class TestaContaBancaria {
+    public static void main(String[] args) {
+        ContaBancaria c1 = new ContaBancaria();
+
+        c1.numero = 1234;
+        c1.titular = "Pedro Silva";
+        c1.saldo = 98745;
+
+        c1.Depositar(1000);
+        c1.Sacar(41723);
+        c1.ConsultarSaldo();
+    }
+}
